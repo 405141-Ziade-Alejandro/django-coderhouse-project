de0f5e172a90ce1f7e821aba751747c8e0f2449e
AlejandroZ <alejandroziade@gmail.com>
2026-03-12 21:11:31 -0300

    added models

---
+++ it_inventory/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ItInventoryConfig(AppConfig):
+    name = 'it_inventory'
---
+++ it_inventory/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class insumo(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.category} {self.name}"
+class user(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+class work_station(models.Model):
+    ws_number = models.CharField(max_length=200)
+    origin = models.CharField(max_length=500)
+    state = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    date_received = models.DateField()
+
+
+    def __str__(self):
+        return self.ws_number
